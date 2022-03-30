61e37b3f747f3e096594608d2f42bb0ab7d7b179
shaun-davies <shaundavieswork@outlook.com>
2022-03-30 12:35:16 +0000

    Add start button function

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,13 @@ let playerSequence = [];
 let click = -1;
 let score = 0;
 let highScore = 0;
+
+/*adds event listener to button to start the game*/
+startButton.addEventListener('click', (e) => {
+    scoreCounter.innerText = "0";
+    sequence = [];
+    playerSequence = [];
+    if(score <= 0) {
+    newSequence();
+    }
+});
