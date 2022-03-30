1e77383aefca0b0287099e6218d4d379ad33ef30
shaun-davies <shaundavieswork@outlook.com>
2022-03-30 12:33:30 +0000

    Add initial JS

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+const infoButton = document.getElementById("info-button");
+const startButton = document.getElementById("start-button");
+const scoreCounter = document.getElementById("score");
+const icons = ['chicken', 'cow', 'pig', 'sheep'];
+
+let sequence = [];
+let playerSequence = [];
+let click = -1;
+let score = 0;
+let highScore = 0;
